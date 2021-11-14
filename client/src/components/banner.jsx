ad9b58321435a6f1ee3710aba5504e6b98c097b1
Edward <jaysongraphics@gmail.com>
2021-11-13 19:29:33 -0500

    changes

--- client/src/components/banner.jsx
+++ client/src/components/banner.jsx
@@ -9,7 +9,7 @@ function Banner() {
             <div>
                 <img className ="bannerimg" src={imageone} alt="meditateimg"/>
             </div>
-
+            {/* intro paragraph */}
                 <div className="banner-div">
                     <p className="banner-title">Breaking Taboo</p>
                     <p className="banner-paragraph">Our intention is to educate, share, and encourage open conversations about this topic. We believe that in order to solve a problem, we must focus on the root cause. In order to save lives, we must kill the silence. In order to kill the silence we must break the taboo. 
